dd7982509ca8dbb16250f7c4516c44da890bb9f3
Eze Onyemaechi <105824065+iamExony@users.noreply.github.com>
2023-09-07 18:52:59 +0100

    edited

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-
 const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const currentDate = new Date();
-const currentDayIndex = currentDate.getDay();
-const currentDayOfWeek = daysOfWeek[currentDayIndex];
+        const currentDate = new Date();
+        const currentDayIndex = currentDate.getDay();
+        const currentDayOfWeek = daysOfWeek[currentDayIndex];
 
-// Update the content of the 'currentDayOfTheWeek' element
-document.getElementById("dayOfWeek").textContent = "Eze";
+        // Update the content of the 'currentDayOfTheWeek' element
+        document.getElementById("dayOfWeek").textContent = currentDayOfWeek;
+
+            // Update the current time
+            function updateCurrentTime() {
+            const currentTimeElement = document.getElementById("currentTime");
+            const currentDate = new Date();
+            const currentUTCTime = currentDate.getTime(); // Get current time in milliseconds
+
+            // Update the content of the 'currentUTCTime' element
+            currentTimeElement.textContent = currentUTCTime;
+        }
+
+        // Update the current time initially
+        updateCurrentTime();
